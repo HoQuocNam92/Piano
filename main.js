8e45e96332db5bef3c0ae0f69570611a7e1107b3
hoquocnam92 <hoquocnam92@gmail.com>
2025-03-08 16:48:59 +0700

    DONE BARS

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 function scrollToTop() {
-    window.scrollTo({top:0 , behavior: 'smooth'});
+  window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
- 
+const bars = document.querySelector(".bars");
+const nav = document.querySelector(".menu-list-mobile");
+
+bars.addEventListener("click", () => {
+  nav.classList.toggle("show");
+});
