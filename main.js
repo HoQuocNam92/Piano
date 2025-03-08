65b5ce4679549a82fff4b5932b2430bbb32bc133
hoquocnam92 <hoquocnam92@gmail.com>
2025-03-08 17:43:16 +0700

    COMMIT

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const bars = document.querySelector(".bars");
+const bar = document.querySelector(".bars");
 const nav = document.querySelector(".menu-list-mobile");
 
-bars.addEventListener("click", () => {
+bar.addEventListener("click", () => {
   nav.classList.toggle("show");
 });
