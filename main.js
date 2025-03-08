4b87096d7b9f8d05a4fb922fd27a006a41a3fc1e
hoquocnam92 <hoquocnam92@gmail.com>
2025-03-08 17:33:53 +0700

    Up

--- main.js
+++ main.js
@@ -4,7 +4,3 @@ const nav = document.querySelector(".menu-list-mobile");
 bars.addEventListener("click", () => {
   nav.classList.toggle("show");
 });
-
-bars.addEventListener("touchstart", () => {
-  nav.classList.toggle("show");
-});
